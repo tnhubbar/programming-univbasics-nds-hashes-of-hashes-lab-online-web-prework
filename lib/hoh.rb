71754658965933b271a7c77a47ea5e35e28e8bb2
Taryn Johnson <taryn.johnson919@gmail.com>
2019-12-07 01:18:13 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -62,4 +62,20 @@ def naming_system
       :Species => {}
     }
   }
+  
+   {
+    label: "Kingdom",
+    sub_category: {
+      label: "Phylum",
+      sub_category: {
+        label: "Class",
+        sub_category: {
+          label: "Order",
+          sub_category: {
+            label: "Family",
+            sub_category: {
+              label: "Genus",
+              sub_category: {
+                label: "Species",
+                sub_category: nil } } } } } } }
 end
